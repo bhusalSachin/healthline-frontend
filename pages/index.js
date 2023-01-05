5d831da3052438bc9331c899e8ccadac24f5cc97
2bitCode <sachinb1377@gmail.com>
2023-01-05 23:41:56 +0545

    initial setup using next js and tailwind

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center border">
+      <div>This is our main client side</div>
+      <div>Welcome</div>
+    </div>
+  );
+}
